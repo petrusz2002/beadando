0ed718f5113f952f7f862f154aa8cf764f8a64d3
petrusz2002 <petreusz2002@gmail.com>
2022-11-07 14:21:15 +0100

    kiegészítés a bemutató modulhoz

--- bemutatando_modul.py
+++ bemutatando_modul.py
@@ -3,12 +3,30 @@ from tkinter import ttk
 from tkinter.messagebox import showinfo
 
 
-class Kerdoiv:
+class sajat_osztaly:
     def megjelenetes(*args):
-        showinfo(
-            title='Eredmény',
-            message=f'Ezt a hónapot választottad {valasztott_honap.get()}!'
-        )
+        index =honap_cb.current()
+        print(index)
+        if index in range(2) or index == 11:
+            showinfo(
+                title='Téli hónap',
+                message=f'Választásod alapján ebben a hónapban születtél: {valasztott_honap.get()}!\n Jó hideg lehetett :,('
+            )
+        elif index in range(2,5):
+            showinfo(
+                title='Téli hónap',
+                message=f'Választásod alapján ebben a hónapban születtél: {valasztott_honap.get()}!\n Szépen kinyílhattak a virágok!'
+            )
+        elif index in range(5,8):
+            showinfo(
+                title='Téli hónap',
+                message=f'Választásod alapján ebben a hónapban születtél: {valasztott_honap.get()}!\n Nagyon meleg van ekkor!'
+            )
+        elif index in range(8,11):
+            showinfo(
+                title='Téli hónap',
+                message=f'Választásod alapján ebben a hónapban születtél: {valasztott_honap.get()}!\n Jó kis szüreti időszakok :,)'
+            )
 
 
 
@@ -20,7 +38,7 @@ root.resizable(False, False)
 root.title('Combobox bemutatás')
 
 # label létrehozása
-lbl = ttk.Label(text="Kérlek válassz egy hónapot:")
+lbl = ttk.Label(text="Melyik hónapban születtél?:")
 lbl.pack(fill=tk.X, padx=5, pady=5)
 
 # combobox létrehozása
@@ -28,10 +46,10 @@ valasztott_honap = tk.StringVar()
 honap_cb = ttk.Combobox(root, textvariable=valasztott_honap)
 
 
-honap_cb['values'] = ["Január","Február","Március",
-                      "Április","Május","Június",""
-                      "Július","Augusztus","Szeptember",
-                      "Október","November","December"]
+honap_cb['values'] = ['Január','Február','Március',
+                      'Április','Május','Június',
+                      'Július','Augusztus','Szeptember',
+                      'Október','November','December']
 
 
 honap_cb['state'] = 'readonly'
@@ -42,7 +60,7 @@ honap_cb.pack(fill=tk.X, padx=5, pady=5)
 
 # esemény amikor változik az érték
 def month_changed(event):
-    Kerdoiv.megjelenetes()
+    sajat_osztaly.megjelenetes()
 
 
 honap_cb.bind('<<ComboboxSelected>>', month_changed)
--- sajat_modul.py
+++ sajat_modul.py
@@ -1,7 +1,7 @@
 def oszsehas(a,b):
     if a < b:
         print("B nagybobb mint A")
-    elif b<a :
+    elif b < a:
         print("A nagybobb mint B")
     else:
         print("A két szám egyenlő")
@@ -10,3 +10,10 @@ def oszsehas(a,b):
         print("B közelebb van az 50-hez")
     elif (50-b) > (50-a):
         print("A közelebb van az 50-hez")
+
+print("Kérek egy számot: ")
+szam1 = int(input())
+print("Kérek még egy számot: ")
+szam2 = int(input())
+
+oszsehas(szam1,szam2)
