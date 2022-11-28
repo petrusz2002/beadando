c6932106e3444b38d9da676a54fbe89dcad553d5
petrusz2002 <petreusz2002@gmail.com>
2022-11-28 09:23:52 +0100

    Kommentek kiszedése, segéd kiíratások kivétele

--- bemutatando_modul.py
+++ bemutatando_modul.py
@@ -6,25 +6,25 @@ from tkinter.messagebox import showinfo
 class sajat_osztaly:
     def megjelenetes(*args):
         index =honap_cb.current()
-        print(index)
+
         if index in range(2) or index == 11:
             showinfo(
-                title='Téli hónap',
+                title='Téli hónapok',
                 message=f'Választásod alapján ebben a hónapban születtél: {valasztott_honap.get()}!\n Jó hideg lehetett :,('
             )
         elif index in range(2,5):
             showinfo(
-                title='Téli hónap',
+                title='Tavaszi hónapok',
                 message=f'Választásod alapján ebben a hónapban születtél: {valasztott_honap.get()}!\n Szépen kinyílhattak a virágok!'
             )
         elif index in range(5,8):
             showinfo(
-                title='Téli hónap',
+                title='Nyári hónapok',
                 message=f'Választásod alapján ebben a hónapban születtél: {valasztott_honap.get()}!\n Nagyon meleg van ekkor!'
             )
         elif index in range(8,11):
             showinfo(
-                title='Téli hónap',
+                title='Őszi hónapok',
                 message=f'Választásod alapján ebben a hónapban születtél: {valasztott_honap.get()}!\n Jó kis szüreti időszakok :,)'
             )
 
@@ -32,16 +32,16 @@ class sajat_osztaly:
 
 root = tk.Tk()
 
-# képernyő létrehozása
+
 root.geometry('300x200')
 root.resizable(False, False)
 root.title('Combobox bemutatás')
 
-# label létrehozása
+
 lbl = ttk.Label(text="Melyik hónapban születtél?:")
 lbl.pack(fill=tk.X, padx=5, pady=5)
 
-# combobox létrehozása
+
 valasztott_honap = tk.StringVar()
 honap_cb = ttk.Combobox(root, textvariable=valasztott_honap)
 
@@ -54,11 +54,11 @@ honap_cb['values'] = ['Január','Február','Március',
 
 honap_cb['state'] = 'readonly'
 
-# elehelyezzük
+
 honap_cb.pack(fill=tk.X, padx=5, pady=5)
 
 
-# esemény amikor változik az érték
+
 def honap_valtozik(event):
     sajat_osztaly.megjelenetes()
 
